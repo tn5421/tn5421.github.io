2b6c3a127f0b032d6cc681b025507f782227fd41
Tyler <tn5421@gmail.com>
2018-06-01 16:34:55 -0500

    forgot to import content and navbar definitions to necro1.css

---
+++ marosia/crafting.js
@@ -0,0 +1,106 @@
+var craftFood = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftWeapon = [
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftInvention = [
+	{Name: ''}
+]
+
+var craftBuilding = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftContainer = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftFurniture = [
+	{Name: ''}
+]
+
+var craftTool = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftMachine = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftVehicle = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
+
+var craftIngredient = [
+	{Name: ''}
+]
+
+var craftMetal = [
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''},
+	{Name: ''}
+]
