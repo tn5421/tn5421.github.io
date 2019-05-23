c44c652392aa04d4ad7eabc44793e49055dce782
Tyler <tn5421@gmail.com>
2019-05-23 11:13:26 -0500

    Update mathbot.py
    Added command ADAM which prints the number of days until the next ADAM summoning

--- bvs/mathbot.py
+++ bvs/mathbot.py
@@ -4,17 +4,27 @@ import time
 
 bot = commands.Bot(command_prefix='!')
 
+wk_list = ["Craftworld at War", "AspenStory", "ADAM", "FarmVale", "ForeverQuest"]
+rday = (int(time.strftime("%d")))
+wk_day = rday % 5
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
 async def wkai(ctx):
-    wk_list = ["Craftworld at War", "AspenStory", "ADAM", "FarmVale", "ForeverQuest"]
-    rday = (int(time.strftime("%d")))
-    wk_day = rday % 5
     day_wkai = "Today's WKAI summon is a "
     day_wkai += wk_list[wk_day]
     await ctx.send(day_wkai)
+
+@bot.command()
+async def adam(ctx):
+    adam_wkai = "The next ADAM summoning is in "
+    if wk_day == 2:
+        adam_wkai += "Five Days"
+    else:
+        adam_wkai += (str((2 - rday) % 5)) + " Days"
+    await ctx.send(adam_wkai)
 
 bot.run("a token")
